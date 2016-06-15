9b84fe587330eda6fc4588882fe27ffb79c162ce
Chris Joel <cdata@google.com>
2016-06-15 00:51:42 -0700

    Move client to subdirectory.

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+function resolvePort() {
+  return process.env.PORT || 8080;
+}
+
+function resolveHostname() {
+  return process.env.MEAT_SCOPE_HOSTNAME || '0.0.0.0';
+}
+
+function resolveStaticFolder() {
+  return process.env.MEAT_SCOPE_STATIC_FOLDER || './client';
+}
+
+const staticFolder = resolveStaticFolder();
+const port = resolvePort();
+const hostname = resolveHostname();
+
+app.use(express.static(staticFolder));
+app.get('*', function(req, res, next) {
+  res.sendFile(`${staticFolder}/index.html`);
+});
+
+app.listen(port);
+
+console.log(`Serving ${staticFolder} app on ${hostname}:${port}.`);
