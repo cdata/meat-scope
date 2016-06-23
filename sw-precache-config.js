f09ee9b188326378867100798cf987c16cda0341
Chris Joel <cdata@google.com>
2016-06-23 01:15:58 -0700

    Ensure all necessary files are precached.

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -5,7 +5,10 @@ module.exports = {
     '/bower_components/webcomponentsjs/webcomponents-lite.js',
     '/bower_components/web-animations-js/web-animations.min.js',
     '/bower_components/webrtc-polyfill/index.js',
-    '/src/images/*'
+    '/bower_components/gif.js/dist/libgif.js',
+    '/src/images/*',
+    '/src/meat-scope-app/*.js',
+    '/src/meat-scope-app/meat-scope-media-converter/*.js'
   ],
   navigateFallback: '/index.html'
 };
